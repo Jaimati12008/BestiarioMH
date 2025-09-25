43479a3c71d0b4a3d8c867e4e9e5ff0a87491d54
Jaimati12008 <matias.morales@estudiantes.colegiotgs.cl>
2025-09-25 15:17:27 -0300

    Update malzeno.py

--- malzeno.py
+++ malzeno.py
@@ -1,13 +1,12 @@
 # Importar streamlit
 import streamlit as st
-
-# ---------------------  Contenido del sitio   --------------------------#
+#linea de titulo
 st.header("Malzeno", divider=True)
-
+#linea de imagen
 st.image("malzenoM.jpg", caption="Malzeno")
-
+#linea de audio
 st.audio("malzenoR.mp3")
-
+#linea de escritura
 st.write("**Informacion del Malzeno** | Créditos: https://monsterhunter.fandom.com/es/wiki/Malzeno")
 
 st.markdown(
@@ -31,11 +30,11 @@ st.markdown(
 -La boca del Malzeno no puede cerrarse completamente sin que sus dientes superiores atraviesen su mandíbula inferior.
 
 """)
-
+#linea de video
 st.video("https://youtu.be/oG4rj2Y-QUY?si=F7R3nOWT_Vs4XyD2")
-
+#linea de titulo
 st.header("Informacion para cazadores", divider=True)
-
+#linea de escritura
 col1, col2 = st.columns(2)
 col1.write("Elemento: Draco")
 col1.write("Estado: Plaga de draco, Plaga sangrienta")
@@ -49,7 +48,8 @@ col1.write("Pariente o variacion: Malzeno Primordial")
 col1.write("Hostilidad: Alta")
 col1.write("Partes rompibles:Cabeza, lomo y patas delanteras, cola cercenable")
 col1.write("Recomendacion:evadir y evitar a toda costa, solo para cazadores experimentados")
-
+#linea de imagen
 col2.image("malzenoP.jpg", caption="Malzeno Primordial")
-
+#linea de video
 col2.video("https://youtu.be/7DbTIIiLjTI?si=7gKdXPST7l32v-33")
+
